893bca372e79968cdccfd9604a1a7709d0c370fb
cyrrrchan <crystalchan110@gmail.com>
2017-10-30 04:57:19 -0700

    2017.10.30 i'm dying

---
+++ Assets/__Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private Transform shotPosition;
+
+    private void Start()
+    {
+        shotPosition = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //shotPosition = transform.right * speed;
+	}
+}
---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+
+    public float xMin, xMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    //public int playerId = 0; // The Rewired player id of this character
+
+    public Boundary boundary;
+    public float moveSpeed;
+
+    //private Player player; // The Rewired player
+    //private CharacterController cc;
+    //private Vector3 moveVector;
+
+    public float moveForce = 365f;          // Amount of force added to move the player left and right.
+    public float jumpSpeed;         // Amount of force added when the player jumps.
+
+    public GameObject shot_fire1;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    private bool grounded = false;          // Whether or not the player is grounded.
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
+        //player = ReInput.players.GetPlayer(playerId);
+
+        // Get the character controller
+        //cc = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rb.velocity = movement * moveSpeed;
+        //rb.AddForce(transform.position.x * (moveHorizontal * moveForce), 1.0f, 0.0f);
+
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            1.0f,
+            0.0f
+        );
+
+        if(Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot_fire1, shotSpawn.position, shotSpawn.rotation);
+        }
+
+        if (grounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                //rb.velocity = new Vector3(0.0f, jumpSpeed, 0.0f);
+                rb.AddForce(0.0f, jumpSpeed, 0.0f);
+                grounded = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}
