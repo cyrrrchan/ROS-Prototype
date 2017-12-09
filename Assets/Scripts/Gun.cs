a673879eadb8a02326ed671f4057de02ce9d7217
cyrrrchan <crystalchan110@gmail.com>
2017-12-08 23:32:14 -0800

    2017.12.08 Prototype with 3 fire buttons + sound
    Added 2D platformer pack from asset store. Implemented 2D sprites instead of 3D objects to fix physics.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+	public Rigidbody2D rocket;				// Prefab of the rocket.
+	public float speed = 20f;				// The speed the rocket will fire at.
+
+    public AudioClip _trumpetHit;
+    public AudioClip _comboHit;
+
+
+	private PlayerControl _playerCtrl;		// Reference to the PlayerControl script.
+	private Animator _anim;					// Reference to the Animator component.
+    private AudioSource _audioSource;
+
+
+	void Awake()
+	{
+		// Setting up the references.
+		_anim = transform.root.gameObject.GetComponent<Animator>();
+		_playerCtrl = transform.root.GetComponent<PlayerControl>();
+        _audioSource = GetComponent<AudioSource>();
+	}
+
+
+	void Update ()
+	{
+		// If the square button is pressed...
+		if(Input.GetButtonDown("Fire1"))
+		{
+			// ... set the animator Shoot trigger parameter and play the audioclip.
+			_anim.SetTrigger("Shoot");
+            _audioSource.PlayOneShot(_trumpetHit);
+
+			// If the player is facing right...
+			if(_playerCtrl.facingRight)
+			{
+				// ... instantiate the rocket facing right and set it's velocity to the right. 
+				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+				bulletInstance.velocity = new Vector2(speed, 0);
+			}
+			else
+			{
+				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
+				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
+				bulletInstance.velocity = new Vector2(-speed, 0);
+			}
+		}
+
+        // If the triangle button is pressed...
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            // ... set the animator Shoot trigger parameter and play the audioclip.
+            _anim.SetTrigger("Shoot");
+            _audioSource.PlayOneShot(_trumpetHit);
+
+            // If the player is facing right...
+            if (_playerCtrl.facingRight)
+            {
+                // ... instantiate the rocket facing right and set it's velocity to the right. 
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(-speed, 0);
+            }
+        }
+
+        // If the circle button is pressed...
+        else if (Input.GetButtonDown("Fire3"))
+        {
+            // ... set the animator Shoot trigger parameter and play the audioclip.
+            _anim.SetTrigger("Shoot");
+            _audioSource.PlayOneShot(_comboHit);
+
+            // If the player is facing right...
+            if (_playerCtrl.facingRight)
+            {
+                // ... instantiate the rocket facing right and set it's velocity to the right. 
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(-speed, 0);
+            }
+        }
+    }
+}
--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Boundary
+public class Boundary1
 {
 
     public float xMin, xMax;
@@ -13,24 +13,29 @@ public class PlayerController : MonoBehaviour {
 
     //public int playerId = 0; // The Rewired player id of this character
 
-    public Boundary boundary;
-    public float moveSpeed;
+    public Boundary1 _boundary;
+    [HideInInspector] public bool _facingRight = true;
+    [HideInInspector] public bool _jump = false;
+    public float _moveForce = 365f; // Amount of force added to move the player left and right.
+    public float _maxSpeed = 5f;
+    public float _jumpForce = 1000f; // Amount of force added when the player jumps.
+    public Transform _groundCheck;
 
     //private Player player; // The Rewired player
     //private CharacterController cc;
     //private Vector3 moveVector;
 
-    //public float moveForce = 365f;          // Amount of force added to move the player left and right.
-    public float jumpSpeed;         // Amount of force added when the player jumps.
+    //public float moveForce = 365f;
+    //public float jumpSpeed;
 
-    public GameObject shot_fire1;
-    public Transform shotSpawn;
-    public float fireRate;
-    private float nextFire;
+    public GameObject _shot_fire1;
+    public Transform _shotSpawn;
+    public float _fireRate;
+    private float _nextFire;
 
-    private bool grounded = false;          // Whether or not the player is grounded.
+    private bool _grounded = false;          // Whether or not the player is grounded.
 
-    private Rigidbody rb;
+    private Rigidbody _rb;
 
     void Awake()
     {
@@ -39,7 +44,7 @@ public class PlayerController : MonoBehaviour {
 
         // Get the character controller
         //cc = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
 
     }
 
@@ -55,28 +60,28 @@ public class PlayerController : MonoBehaviour {
         //rb.velocity = movement * moveSpeed;
 		//rb.AddForceAtPosition((moveHorizontal * moveForce), 1.0f, 0.0f);
 		//rb.AddForceAtPosition(new Vector3(1,0,0),transform.position-new Vector3(.5f,0,0));
-		rb.AddForce (movement * moveSpeed);
+		_rb.AddForce (movement * _moveForce);
 
-        rb.position = new Vector3(
-            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+        _rb.position = new Vector3(
+            Mathf.Clamp(_rb.position.x, _boundary.xMin, _boundary.xMax),
             1.0f,
             0.0f
         );
 
-        if(Input.GetButton("Fire1") && Time.time > nextFire)
+        if(Input.GetButton("Fire1") && Time.time > _nextFire)
         {
-            nextFire = Time.time + fireRate;
-            Instantiate(shot_fire1, shotSpawn.position, shotSpawn.rotation);
+            _nextFire = Time.time + _fireRate;
+            Instantiate(_shot_fire1, _shotSpawn.position, _shotSpawn.rotation);
         }
 
-        if (grounded)
+        if (_grounded)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 //rb.velocity = new Vector3(0.0f, jumpSpeed, 0.0f);
-				rb.AddForce(0.0f, jumpSpeed, 0.0f,ForceMode.Impulse);
+				_rb.AddForce(0.0f, _jumpForce, 0.0f,ForceMode.Impulse);
 				//rb.AddExplosionForce (jumpSpeed, transform.position - new Vector3 (0, .5f, 0), 100);
-                grounded = false;
+                _grounded = false;
             }
         }
     }
@@ -85,7 +90,7 @@ public class PlayerController : MonoBehaviour {
     {
         if (collision.collider.tag == "Ground")
         {
-            grounded = true;
+            _grounded = true;
         }
     }
 }
