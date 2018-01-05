a5f0c7ef9da132701188b64025d4448294998536
cyrrrchan <crystalchan110@gmail.com>
2018-01-05 13:04:32 -0800

    2018.01.05 it's not working on my work computer...

--- Assets/__Scripts/PlayerControl.cs
+++ Assets/__Scripts/PlayerControl.cs
@@ -61,8 +61,8 @@ public class PlayerControl : MonoBehaviour
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h));
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        anim.SetFloat("Speed", Mathf.Abs(h));
 
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
 		if(h * _rb2d.velocity.x < maxSpeed)
