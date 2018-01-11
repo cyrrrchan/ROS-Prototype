a08e8de144b6c8d57ff4b290c2c218dada7f9e69
Crystal Chan <crystalchan110@gmail.com>
2018-01-10 18:18:14 -0800

    2018.01.10 Fixed Jumping null error

--- Assets/__Scripts/PlayerControl.cs
+++ Assets/__Scripts/PlayerControl.cs
@@ -41,7 +41,7 @@ public class PlayerControl : MonoBehaviour
 	void Update()
 	{
 		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		//grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
         // If the jump button is pressed and the player is grounded then the player should jump.
         if (Input.GetButtonDown("Jump") && grounded)
