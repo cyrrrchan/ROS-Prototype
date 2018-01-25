a0c44c9bc9c89e2b08891bc6466f970c2ffa4bcb
cyrrrchan <crystalchan110@gmail.com>
2018-01-24 21:50:57 -0800

    2018.01.24 Implemented Interactive Music system

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MUSICLEVELS
+        {
+            static const AkUniqueID GROUP = 4206939811U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID KIDKWEI_LEVEL01 = 397135409U;
+            } // namespace SWITCH
+        } // namespace MUSICLEVELS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID FOUR = 2863728729U;
+        static const AkUniqueID ONE = 1064933119U;
+        static const AkUniqueID THREE = 912956111U;
+        static const AkUniqueID TWO = 678209053U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID KIDKWEI_LEVEL01 = 397135409U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ Assets/__Scripts/RTPCListener.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTPCListener : MonoBehaviour
+{
+
+    //set your RTPCID to the name of your desired gameparameter (under GameSyncs)
+    public string _rtpcID_One;
+    public string _rtpcID_Two;
+    public string _rtpcID_Three;
+    public string _rtpcID_Four;
+
+    private void Start()
+    {
+        // make sure all RTPC values are set to 0
+        AkSoundEngine.SetRTPCValue(_rtpcID_One, 0.0f);
+        AkSoundEngine.SetRTPCValue(_rtpcID_Two, 0.0f);
+        AkSoundEngine.SetRTPCValue(_rtpcID_Three, 0.0f);
+        AkSoundEngine.SetRTPCValue(_rtpcID_Four, 0.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        // RTPCValue_type.RTPCValue_Global
+        // for whatever reason, this constant isn't exposed by name by WWise C#/Unity headers
+        //int type = 1;
+
+        // will contain the value of the RTPC parameter after the GetRTPCValue call
+        //float value;
+
+        // retrieves current value of the RTPC parameter and stores it in 'value'
+        //AkSoundEngine.GetRTPCValue(rtpcID_One, gameObject, 0, out value, ref type);
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            AkSoundEngine.SetRTPCValue(_rtpcID_One, 100f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Two, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Three, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Four, 0.0f);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            AkSoundEngine.SetRTPCValue(_rtpcID_One, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Two, 100f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Three, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Four, 0.0f);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            AkSoundEngine.SetRTPCValue(_rtpcID_One, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Two, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Three, 100f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Four, 0.0f);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            AkSoundEngine.SetRTPCValue(_rtpcID_One, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Two, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Three, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Four, 100f);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            AkSoundEngine.SetRTPCValue(_rtpcID_One, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Two, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Three, 0.0f);
+            AkSoundEngine.SetRTPCValue(_rtpcID_Four, 0.0f);
+        }
+
+
+        //
+        // use 'value' here
+        //
+        // e.g. transform.localScale += Vector3( value, 0, 0 );
+        // which would scale by the value of the RTPC parameter
+    }
+}
