5c631a702ddae8a04a77b0126ce85b710a005c5a
Crystal Chan <crystalchan110@gmail.com>
2018-02-15 23:14:13 -0800

    2018.02.15

---
+++ Assets/__Scripts/OneManBand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneManBand : Enemy {
+
+	enum State {Idle,
+		Taunt,
+		Attack};
+	
+	// Use this for initialization
+	void Start () {
+		State state; 
+
+		state = State.Idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
